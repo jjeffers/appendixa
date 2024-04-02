3b944e46b2a6971ead0102b8b62b0f2498a2651d
jjeffers <jjeffers@users.noreply.github.com>
2024-04-02 11:40:47 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jjeffers"
+original_name="appendixa"
+original_urlname="appendixa"
+original_description="Awesome appendixa created by jjeffers"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv appendixa $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ appendixa/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for appendixa."""
+
+from appendixa.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
